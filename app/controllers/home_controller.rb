b0feabf2595f3f035960107420e0d4981717a951
Evan <evjay.a@gmail.com>
2025-03-27 22:29:26 -0600

    Add delete family confirmation modal with Alpine.js and styled warnings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in? && current_user.family
+      redirect_to family_path(current_user.family)
+    end
   end
 
   def about
