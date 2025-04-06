096d943772d3ff0ecbe3123f95bb2c962f86ec97
Evan <evjay.a@gmail.com>
2025-04-06 11:06:54 -0600

    added heroku gem and prepping for deployment for beta

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "family_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'yourdomain.com', protocol: 'https' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
